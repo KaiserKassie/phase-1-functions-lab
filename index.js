c0f1dfd54d5b935dbc1a0c535d318773a26e62cf
Kassie Becker <kassaundralee@hotmail.com>
2022-02-23 18:47:15 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = function (distance) {
+    return Math.abs(42 - distance)
+}
+
+function distanceFromHqInFeet(distance) {
+    let blockDistance = distanceFromHqInBlocks(distance);
+    return blockDistance * 264
+}
+
+const distanceTravelledInFeet = function (start, destination) {
+    return Math.abs(start-destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetDistance = distanceTravelledInFeet(start, destination);
+    if (feetDistance > 400 && feetDistance <= 2000) {
+        return ((feetDistance - 400) * .02)
+    } else
+    if (feetDistance > 2000 && feetDistance <= 2500) {
+        return 25
+    } else
+    if (feetDistance > 2500) {
+        return "cannot travel that far"
+    } else {
+        return 0
+    }
+}
